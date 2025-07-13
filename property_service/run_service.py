e9c4ac0143a6fcf6fd04a1f9a7a6dc6e735f60bb
sairam <zameenpecharcha@gmail.com>
2025-07-13 15:53:22 +0530

    property_service uvicorn changes and saving to db

--- property_service/run_service.py
+++ property_service/run_service.py
@@ -1,4 +1,11 @@
-import uvicorn
+import os
+import sys
+
+# Add the current directory to Python path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_dir)
+
+from app.service.property_service import serve
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=True) 
+    serve() 
