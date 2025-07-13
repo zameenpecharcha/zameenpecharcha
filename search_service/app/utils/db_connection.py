833d00cf057daef5c7218b3327305070e6377c13
sairam <zameenpecharcha@gmail.com>
2025-07-13 16:32:20 +0530

    db_connect

--- search_service/app/utils/db_connection.py
+++ search_service/app/utils/db_connection.py
@@ -5,11 +5,11 @@ import os
 
 def get_db_engine():
     load_dotenv()
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT")
-    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_PORT = os.getenv("DB_PORT", "5432")
+    DB_NAME = "postgres"  # Using postgres database directly
     DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     engine = create_engine(DATABASE_URL, echo=True)
     print("Connected to PostgreSQL database successfully!")
