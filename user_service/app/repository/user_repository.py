b0b73e303ec3c4c6000b1d6cfbffec47989b8a14
sairam <zameenpecharcha@gmail.com>
2025-07-13 20:55:57 +0530

    added_location_details to the user service

--- user_service/app/repository/user_repository.py
+++ user_service/app/repository/user_repository.py
@@ -6,12 +6,14 @@ from sqlalchemy import select
 SessionLocal = sessionmaker(bind=get_db_engine())
 
 class UserRow:
-    def __init__(self, user_id, name, email, phone, password):
+    def __init__(self, user_id, name, email, phone, password, role, location):
         self.user_id = int(user_id)
         self.name = str(name)
         self.email = str(email)
         self.phone = int(phone) if phone else None
         self.password = str(password)
+        self.role = str(role) if role else None
+        self.location = str(location) if location else None
 
 def get_user_by_id(id):
     session = SessionLocal()
@@ -21,7 +23,9 @@ def get_user_by_id(id):
             users.c.name,
             users.c.email,
             users.c.phone,
-            users.c.password
+            users.c.password,
+            users.c.role,
+            users.c.location
         ).where(users.c.user_id == id)
         result = session.execute(query).fetchone()
         if result:
@@ -30,11 +34,16 @@ def get_user_by_id(id):
     finally:
         session.close()
 
-def create_user(name, email, phone, password):
+def create_user(name, email, phone, password, role=None, location=None):
     session = SessionLocal()
     try:
         result = session.execute(users.insert().returning(users.c.user_id).values(
-            name=name, email=email, phone=phone, password=password
+            name=name,
+            email=email,
+            phone=phone,
+            password=password,
+            role=role,
+            location=location
         ))
         session.commit()
         return result.scalar()  # This will return the inserted user_id
